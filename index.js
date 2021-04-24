62be50fe8d85f6fd89ce26c5d9d8513acf46cd8c
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-04-25 02:22:08 +0900

    メッセージをFavorite!に変更

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const favoriteButtons = document.getElementsByClassName('favorite-button');
 
 for (let index = 0; index < favoriteButtons.length; index++) {
     favoriteButtons[index].addEventListener("click", () => {
-        console.log("Yes!");
+        console.log("Favorite!");
     });
 };
