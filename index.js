abb0f272051d6ee69320c970efeaa6b21a28381a
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-04-25 14:36:31 +0900

    色の調整、すべてのボタンに色効果を適用

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const favoriteButtons = document.getElementsByClassName('favorite-button');
 for (let index = 0; index < favoriteButtons.length; index++) {
     favoriteButtons[index].addEventListener("click", () => {
         console.log("Favorite!");
-        favoriteButtons[index].style.color = '#ff1493';
+        favoriteButtons[index].style.color = '#ff69b4';
         favoriteButtons[index].style.border = 'solid 1px #ff69b4';
     });
 };
