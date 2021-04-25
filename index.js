fa095ef16f192bf4b7753953a2a481ebc4dba2b3
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-04-25 14:31:25 +0900

    ボタンの枠の色も変更するように変更

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const favoriteButtons = document.getElementsByClassName('favorite-button');
 for (let index = 0; index < favoriteButtons.length; index++) {
     favoriteButtons[index].addEventListener("click", () => {
         console.log("Favorite!");
-        favoriteButtons[index].style.color='#ff1493';
+        favoriteButtons[index].style.color = '#ff1493';
+        favoriteButtons[index].style.border-color = '#ff69b4';
     });
 };
 
