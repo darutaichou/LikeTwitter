37329223d465408e4c90293091674462a399dbf1
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-04-25 19:52:52 +0900

    色をRGBで判定して変更。てすと

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const favoriteButtons = document.getElementsByClassName('favorite-button');
 // ツイートボタン
 const tweetButton = document.getElementById('tweet-button');
-console.log(tweetButton);
 
 // お気に入りボタンを押すとボタンが赤くなる効果
 // idを "favorite-button-pink" に変更
@@ -10,9 +9,11 @@ for (let index = 0; index < favoriteButtons.length; index++) {
     const favoriteButton = favoriteButtons[index];
     if (favoriteButton.id == 'favorite-button' || favoriteButton.id == 'favorite-button-gray') {
         favoriteButton.addEventListener("click", () => {
+            console.log(favoriteButton.style.color);
             favoriteButton.style.color = '#ff69b4';
             favoriteButton.style.borderColor = '#ff69b4';
             favoriteButton.id = 'favorite-button-pink'
+            console.log(favoriteButton.style.color);
         });
     }
 };
@@ -21,11 +22,13 @@ for (let index = 0; index < favoriteButtons.length; index++) {
 // idを "favorite-button-gray" に変更
 for (let index = 0; index < favoriteButtons.length; index++) {
     const favoriteButton = favoriteButtons[index];
-    if (favoriteButton.id == 'favorite-button-pink') {
+    if (favoriteButton.style.color == 'rgb(255, 105, 180)') {
         favoriteButton.addEventListener("click", () => {
+            console.log("OK");
             favoriteButton.style.color = '#c0c0c0';
             favoriteButton.style.borderColor = '#c0c0c0';
             favoriteButton.id = 'favorite-button-gray'
+            console.log(favoriteButton.id);
         });
     }
 }
