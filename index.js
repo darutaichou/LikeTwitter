67e69a6b548bab2fcad48c08bf8065e86e6e01e5
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-04-25 21:06:19 +0900

    スイッチ文でフラグを判定することで実装に成功。

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-// お気に入りボタン
-const favoriteButtons = document.getElementsByClassName('favorite-button');
 // ツイートボタン
 const tweetButton = document.getElementById('tweet-button');
+// お気に入りボタン
+const favoriteButtons = document.getElementsByClassName('favorite-button');
+// お気に入りボタンの色フラグ
+let pinkOrGray = "gray";
 
-// お気に入りボタンを押すとボタンが赤くなる効果
-// idを "favorite-button-pink" に変更
+// お気に入りボタンを押すとボタンの色が赤くなる
+// お気に入りボタンをもう一度押すとボタンの色がグレーに戻る
 for (let index = 0; index < favoriteButtons.length; index++) {
     const favoriteButton = favoriteButtons[index];
-    if (favoriteButton.id == 'favorite-button' || favoriteButton.id == 'favorite-button-gray') {
-        favoriteButton.addEventListener("click", () => {
-            console.log(favoriteButton.style.color);
-            favoriteButton.style.color = '#ff69b4';
-            favoriteButton.style.borderColor = '#ff69b4';
-            favoriteButton.id = 'favorite-button-pink'
-            console.log(favoriteButton.style.color);
-        });
-    }
+    favoriteButton.addEventListener("click", () => {
+        console.log(pinkOrGray);
+        switch (pinkOrGray) {
+            case "gray":
+                favoriteButton.style.color = '#ff69b4';
+                favoriteButton.style.borderColor = '#ff69b4';
+                pinkOrGray = "pink";
+                break;
+            case "pink":
+                favoriteButton.style.color = '#c0c0c0';
+                favoriteButton.style.borderColor = '#c0c0c0';
+                pinkOrGray = "gray";
+                break;
+        }
+    });
 };
-
-// お気に入りボタンをもう一度押すとボタンの色が戻る
-// idを "favorite-button-gray" に変更
-for (let index = 0; index < favoriteButtons.length; index++) {
-    const favoriteButton = favoriteButtons[index];
-    if (favoriteButton.style.color == 'rgb(255, 105, 180)') {
-        favoriteButton.addEventListener("click", () => {
-            console.log("OK");
-            favoriteButton.style.color = '#c0c0c0';
-            favoriteButton.style.borderColor = '#c0c0c0';
-            favoriteButton.id = 'favorite-button-gray'
-            console.log(favoriteButton.id);
-        });
-    }
-}
 
 // ツイートボタンを押すとボタンの色が暗くなる
 tweetButton.addEventListener("mousedown", () => {
