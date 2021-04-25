7f97e94cac004599af31c1832e709ec3c114b6ce
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-04-25 18:53:12 +0900

    お気に入りボタンをもう一度押すと色が戻る効果を適用するが、戻らない。

--- index.js
+++ index.js
@@ -9,21 +9,23 @@ for (let index = 0; index < favoriteButtons.length; index++) {
     favoriteButtons[index].addEventListener("click", () => {
         console.log("Favorite!");
         favoriteButtons[index].style.color = '#ff69b4';
-        favoriteButtons[index].style.border = 'solid 1px #ff69b4';
+        favoriteButtons[index].style.borderColor = '#ff69b4';
     });
 };
 
 // お気に入りボタンをもう一度押すとボタンの色が戻る
 for (let index = 0; index < favoriteButtons.length; index++) {
-    favoriteButtons[index].addEventListener("click", () => {
-        console.log("Favorite!");
-        favoriteButtons[index].style.color = '';
-        favoriteButtons[index].style.border = '';
-    });
-};
+    if (favoriteButtons[index].style.color == '#ff69b4' && favoriteButtons[index].style.borderColor == '#ff69b4') {
+        favoriteButtons[index].addEventListener("click", () => {
+            console.log("Favorite!");
+            favoriteButtons[index].style.color = '#ff69b4';
+            favoriteButtons[index].style.border = 'solid 1px #ff69b4';
+        });
+    }
+}
 
 // ツイートボタンを押すとボタンの色が暗くなる
-tweetButton.addEventListener("mousedown", ()=> {
+tweetButton.addEventListener("mousedown", () => {
     tweetButton.style.backgroundColor = '#4682b4';
 })
 
