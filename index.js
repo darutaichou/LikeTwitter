7c6291bbb5b13c47ed8b1e0e46ca02212e0f0fd6
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-04-25 18:20:49 +0900

    ボタンを離すとボタンの色が戻る効果を適用

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // お気に入りボタン
 const favoriteButtons = document.getElementsByClassName('favorite-button');
 // ツイートボタン
-const tweetButton = document.getElementById('tweet-textbox');
+const tweetButton = document.getElementById('tweet-button');
+console.log(tweetButton);
 
 // お気に入りボタンを押すとボタンが赤くなる効果
 for (let index = 0; index < favoriteButtons.length; index++) {
@@ -13,6 +14,11 @@ for (let index = 0; index < favoriteButtons.length; index++) {
 };
 
 // ツイートボタンを押すとボタンの色が暗くなる
-tweetButton.addEventListener("onmousedown", ()=> {
-    tweetButton.style.backgroundcolor = '#6495ed';
+tweetButton.addEventListener("mousedown", ()=> {
+    tweetButton.style.backgroundColor = '#4682b4';
+})
+
+// ツイートボタンを離すとボタンの色が戻る
+tweetButton.addEventListener("mouseup", () => {
+    tweetButton.style.backgroundColor = '';
 })
