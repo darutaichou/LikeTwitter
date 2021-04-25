bc4ff6ddc8e5e33dcdb01b089b53c02823eb17da
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-04-25 14:29:28 +0900

    ボタンの色を変更するために2

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const favoriteButtons = document.getElementsByClassName('favorite-button');
 for (let index = 0; index < favoriteButtons.length; index++) {
     favoriteButtons[index].addEventListener("click", () => {
         console.log("Favorite!");
+        favoriteButtons[index].style.color='#ff1493';
     });
 };
+
